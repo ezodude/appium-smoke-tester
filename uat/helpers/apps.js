909545f0ee9345a572e6d785e6fd7aaa07152687
Ezo Saleh <ezo@the-experimenters.com>
2014-06-25 07:45:44 +0100

    Removed unnecessary app.

--- uat/helpers/apps.js
+++ uat/helpers/apps.js
@@ -8,7 +8,6 @@ if (process.env.DEV) {
   exports.iosPrivate1Device = "sample-code/apps/ios/device/Private1.app";
   exports.androidPrivate1 = "sample-code/apps/android/Private1-debug.apk";
 } else {
-  exports.iosTestApp = "http://appium.github.io/appium/assets/TestApp7.1.app.zip";
   exports.iosWebviewApp = "http://appium.github.io/appium/assets/WebViewApp7.1.app.zip";
   exports.androidApiDemos = "http://appium.github.io/appium/assets/ApiDemos-debug.apk";
   exports.selendroidTestApp = "http://appium.github.io/appium/assets/selendroid-test-app-0.10.0.apk";
